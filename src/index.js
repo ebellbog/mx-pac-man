491fe80184c8e1d3834a15eb98105b171781dc3d
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2022-02-10 16:36:04 -0500

    support embedding

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import ClassicLevelData from '../data/classicLevel';
 
 $(document).ready(() => {
     const firstLevel = new PacLevel(ClassicLevelData, $('#game'));
+
+    const queryParams = new URLSearchParams(window.location.search);
+    const isEmbedded = Boolean(parseInt(queryParams.get('embedded'))) || (window !== window.top);
+    $('body').toggleClass('embedded', isEmbedded);
 });
